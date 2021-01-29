60c7d65e38e0f1a4190df4edb1cc2b5e6bf5f9c4
George Emmanuel <70173997+George-Emmanuel@users.noreply.github.com>
2021-01-29 15:05:17 +0530

    Create reference_for_alphabets.py

---
+++ reference_for_alphabets.py
@@ -0,0 +1,13 @@
+alpha = []
+Alpha = []
+num = []
+for i in range(26):
+    alpha.append(str(chr(i + 97)))
+print(alpha)  # Lowercase alphabets
+for i in range(26):
+    Alpha.append(str(chr(i + 65)))
+print(Alpha)  # Uppercase alphabets
+for i in range(26):
+    num.append(i)
+print(num)
+print(dict(zip(alpha, num)))
